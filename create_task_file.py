73f4354c656a9a492cadc1004d6c3c8f2e2dc3de
Javad Pourmostafa <javad.pourmostafa@gmail.com>
2024-09-18 10:14:44 +0200

    Create create_task_file.py

---
+++ create_task_file.py
@@ -0,0 +1,101 @@
+from tqdm import tqdm
+import random
+from transformers import XGLMTokenizer, XGLMForCausalLM
+import torch
+from tqdm import tqdm
+import numpy as np
+from transformers import pipeline
+from utils import *
+import time
+from carbontracker.tracker import CarbonTracker
+
+# set the domain
+domain = 'it'
+
+
+eval_src, eval_tgt = get_data(domain, 'de', 'en', "dev", '../datasets')
+train_src, train_tgt = get_data(domain, 'de', 'en', "train", '../datasets')
+
+num_itrs = 100
+k = 1
+prompts = {}
+lengths = [len(x)*2 for x in eval_src]      
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+tokenizer = XGLMTokenizer.from_pretrained("facebook/xglm-7.5B")
+model = XGLMForCausalLM.from_pretrained("facebook/xglm-7.5B", torch_dtype=torch.float16)
+model.half()
+model.to(device)
+model.eval()
+
+predictions = []
+scores = {}
+scores.setdefault("id", [])
+scores.setdefault("score", [])
+
+# Carbon tracker
+tracker = CarbonTracker(epochs=2000, monitor_epochs=-1, epochs_before_pred = -1, verbose=2, components="gpu", log_dir=f'decoding/{domain},')
+
+# Record the start time
+start_time = time.time()
+
+#tracker starts
+tracker.epoch_start()
+
+
+for it in range(num_itrs):
+    randomSeed = it
+    random.seed(randomSeed)
+    RandomNumber = random.sample(range(len(train_src)), k)
+    print("Random number(s) for this iteration: ", RandomNumber)
+    for i, sample in tqdm(enumerate(eval_src)):
+        if len(RandomNumber)==1:
+             prompts[it, i] = train_src[RandomNumber[0]] + " = " + train_tgt[RandomNumber[0]] + " </s> " + sample + " = "
+        if len(RandomNumber)>1:
+             prompts[it, i] = (" </s> ").join([f"{train_src[item]} = {train_tgt[item]}" for item in RandomNumber])
+             prompts[it, i] = prompts[it, i] + " </s> " + sample + " = "
+        input_ids = tokenizer.encode(prompts[it, i], return_tensors='pt').to(device)
+        output = model.generate(input_ids, max_new_tokens=200, do_sample=False)
+        final_output = tokenizer.decode(output[0, input_ids.shape[1]: ], skip_special_tokens=True)
+        predictions.append(final_output)
+        with open(f"{domain}{'/task-level-XGLM-7.5b-'}{k}{'+0'}.{it}.new", "a") as f:
+            f.write(final_output + "\n")
+        print("-------")
+        print("trial: ", it, ", iterator in loop: ", i)
+        print("prompt: ", prompts[it, i])
+        print("model ouput: ", final_output)
+        print("actual label: ", eval_tgt[i])
+        outputs = get_outputs(predictions, truncate=True, max_length=lengths[:i+1])
+        print("bleu score so far: ", score(outputs, eval_tgt[:i+1]))
+        # print("sacrebleu score so far: ", score_sacrebleu(outputs, eval_tgt[:j+1]))
+    with open(f"{domain}{'/task-level-XGLM-7.5b-'}{k}{'+0'}.{it}.new.{'bleu'}", "w") as f:
+            f.write(str(score(outputs, eval_tgt)))
+    scores["id"].append(it)
+    scores["score"].append(score(outputs, eval_tgt)['score'])
+    print(score(outputs, eval_tgt))
+    predictions = []
+    print("the id for the best-performing sample so far is:", scores["id"][np.argmax(scores["score"])])
+
+print("the id for the best-performing sample is:", scores["id"][np.argmax(scores["score"])])
+
+
+# tracker ends
+tracker.epoch_end()
+
+# Record the end time
+end_time = time.time()
+
+# Calculate the total elapsed time
+total_elapsed_time = end_time - start_time
+
+# Save the total elapsed time
+print(f"Total Elapsed Time: {total_elapsed_time} seconds")
+output_file_path = f"{domain}{'/task-level-XGLM-7.5b-'}{k}{'+0'}.{it}.new.TT"
+with open(output_file_path, "a") as output_file:
+    output_file.write(f"{total_elapsed_time}\n")
+
+# tracker stops
+tracker.stop()
+   
+
+
